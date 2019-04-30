6504b888e4cd74c1301f81b7e5fe325da5ae5b6a
ShaoEnChen <seanchen47@gmail.com>
2019-04-29 20:54:43 -0500

    add a verification feature for GUI to show a sample dog of the breed predicted

--- gui/gui.py
+++ gui/gui.py
@@ -1,15 +1,16 @@
 # -*- coding: utf-8 -*-
+import os
 import tkinter as tk
-from tkinter.filedialog import askopenfilename
-from PIL import Image, ImageTk
 import numpy as np
 import json
+from tkinter.filedialog import askopenfilename
+from PIL import Image, ImageTk
 from keras.models import load_model
 
 img_size = 224
-text_wrap_length = 840
 
-model_file = 'model.h5'
+# https://drive.google.com/file/d/1RasU0buZ00D6BpYia8IFlMaf0jWnwRSJ/view?usp=sharing
+model_file = 'CSCE636_final_model.h5'
 uploaded_img = None
 breeds = {}
 
@@ -21,15 +22,17 @@ for key, val in classes.items():
     breeds[val] = key
 
 def rescaleImg(img):
-    # Scale and center image to fit canvas (longer side has length 300)
+    # Scale and center image to fit canvas (longer side has length img_size(300))
+    global img_size
     w, h = img.size
+    scaled_w = img_size
     if w > h:
-        scale = 300 / w
+        scale = scaled_w / w
         x_shift = 0
-        y_shift = int(150 - h * scale / 2)
+        y_shift = int(scaled_w / 2 - h * scale / 2)
     else:
-        scale = 300 / h
-        x_shift = int(150 - w * scale / 2)
+        scale = scaled_w / h
+        x_shift = int(scaled_w / 2 - w * scale / 2)
         y_shift = 0
 
     rescaled_img = img.resize((int(w * scale), int(h * scale)))
@@ -51,6 +54,19 @@ def uploadImg():
     imgfile = ImageTk.PhotoImage(imgfile)
     image_canvas.image = imgfile
     image_canvas.create_image(x_shift, y_shift, anchor = 'nw', image = imgfile)
+    image_canvas.delete(img_display_instruction)
+
+def updateSampleImg(breed):
+    sample_dir = os.path.join('./', breed)
+    try:
+        img = Image.open(os.path.join(sample_dir, 'sample.jpg'))
+    except:
+        return
+
+    imgfile, x_shift, y_shift = rescaleImg(img)
+    imgfile = ImageTk.PhotoImage(imgfile)
+    intro_canvas.image = imgfile
+    intro_canvas.create_image(x_shift, y_shift, anchor = 'nw', image = imgfile)
 
 def predict():
     global uploaded_img
@@ -65,12 +81,13 @@ def predict():
     model = load_model(model_file)
     breed_idx = model.predict_classes(img_array)[0]
 
-    introduction.configure(text = "This is a %s !" %(breeds[breed_idx]))
+    intro.configure(text = "This is a %s ! A dog of this type looks like:" %(breeds[breed_idx]), pady = 20)
+    updateSampleImg(breeds[breed_idx])
 
 # Initiate the window
 window = tk.Tk()
 window.title('Dog Breed Classification')
-window.geometry('960x540')
+window.geometry('768x700')
 
 # ROW 1
 # Set title in the window content
@@ -83,7 +100,7 @@ images_frame = tk.Frame(window)
 images_frame.pack()
 
 image_canvas = tk.Canvas(images_frame, width = img_size, height = img_size, bd = 0)
-image_canvas.create_text(150, 150, text = 'Your image will be displayed here.')
+img_display_instruction = image_canvas.create_text(int(img_size / 2), int(img_size / 2), text = 'Your image will be displayed here.')
 image_canvas.pack()
 
 # ROW 3
@@ -91,7 +108,7 @@ image_canvas.pack()
 buttons_frame = tk.Frame(window)
 buttons_frame.pack()
 
-upload_image_button = tk.Button(buttons_frame, text = 'Open', pady = 20, command = uploadImg)
+upload_image_button = tk.Button(buttons_frame, text = 'Upload', pady = 20, command = uploadImg)
 upload_image_button.pack(side = 'left')
 
 predict_button = tk.Button(buttons_frame, text = 'Predict', pady = 20, command = predict)
@@ -99,7 +116,13 @@ predict_button.pack(side = 'right')
 
 # ROW 4
 # Set introductions
-introduction = tk.Label(window, wraplength = text_wrap_length, text = 'Please upload an image to classify.')
-introduction.pack()
+intro_frame = tk.Frame(window)
+intro_frame.pack()
+
+intro = tk.Label(intro_frame, wraplength = 840, text = 'Please upload an image of A DOG to classify.')
+intro.pack()
+
+intro_canvas = tk.Canvas(intro_frame, width = img_size, height = img_size, bd = 0)
+intro_canvas.pack()
 
 window.mainloop()
